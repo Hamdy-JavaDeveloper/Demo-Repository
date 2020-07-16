54a5929f2a9063fa2f63ec0f11e6abb1b3e92a5d
hamdi <hamdi@hamdi01>
2020-07-16 21:43:17 +0300

    opoppo

---
+++ DemoApplication/src/main/java/com/demo/controller/AbstractController.java
@@ -0,0 +1,52 @@
+/*
+ * package com.demo.controller;
+ * 
+ * import java.awt.event.ActionEvent; import java.io.IOException; import
+ * java.util.ArrayList; import java.util.List;
+ * 
+ * import com.demo.bean.ItemUnit;
+ * 
+ * import javafx.collections.FXCollections; import
+ * javafx.collections.ObservableList; import javafx.fxml.FXML; import
+ * javafx.scene.control.Button; import javafx.scene.control.TableColumn; import
+ * javafx.scene.control.TableView; import
+ * javafx.scene.control.cell.PropertyValueFactory;
+ * 
+ * public abstract class AbstractController {
+ * 
+ * // StageManager stageManager;
+ * 
+ * @FXML private TableView<ItemUnit> tbItemUnit;
+ * 
+ * @FXML private TableColumn<ItemUnit, String> colUnitName;
+ * 
+ * @FXML private TableColumn<ItemUnit, Double> colUnitQty;
+ * 
+ * @FXML private TableColumn<ItemUnit, Double> colUnitPrice;
+ * 
+ * @FXML private Button btnItemAddUnit;
+ * 
+ * @FXML private Button btnItemDeleteUnit;
+ * 
+ * @FXML private Button btnItemUpdateUnitPrice; ObservableList<ItemUnit>
+ * unitsData=FXCollections.observableArrayList();
+ * 
+ * public void initializeUnitTable() {
+ * 
+ * colUnitName.setCellValueFactory(new PropertyValueFactory<>("unit"));
+ * colUnitQty.setCellValueFactory(new PropertyValueFactory<>("pieces"));
+ * colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+ * 
+ * List<ItemUnit> units=new ArrayList<ItemUnit>();
+ * 
+ * .
+ * 
+ * 
+ * }//end method
+ * 
+ * @FXML public void addUnit(ActionEvent e) throws IOException {
+ * 
+ * //stageManager.switchScene(FxmlView.TEST);
+ * 
+ * } }
+ */
